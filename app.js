4890ddce9575837fde9cb459a948a92d54d95b5e
= <=>
2020-05-19 08:12:19 +0530

    2p live game

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const logger = require('morgan');
+const socket = require('socket.io');
+const port = 1234;
+
+const app = express();
+
+app.use(logger('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res)=>{
+    res.render('tictactoe');
+})
+
+const server = app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
+var io = socket(server);
+
+io.on('connection', (socket)=>{
+    console.log('made socket connection');
+    console.log(socket.id);
+
+    socket.on('play', (data) => {
+        console.log(data);
+        io.sockets.emit('play', data);
+    });
+
+    socket.on('restart', (data)=> {
+        io.sockets.emit('restart', data);
+    })
+});
